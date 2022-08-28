cca54ca9243b6762054f52f1bdb07158a8e82102
zerojuu <zerojuu@naver.com>
2022-08-28 20:00:19 +0900

    상품 엔티티 개발(+비즈니스 로직)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter  //@Setter
 //abstract 추상 클래스
 public abstract class Item {
 
@@ -26,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //데이터(stockQuantity)를 가지고 있는 쪽(item)에 비즈니스로직을 놓는 게 응집력 좋음
+    //stockQuantity 값을 변경할 일이 있으면 setter가 아닌 비즈니스로직으로 하는 게 좋음
+    /**
+     *  stock(재고수량) 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity = quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
